3a7f0e29f07917bba9050380d5ec48767d499fc4
Adriana <dri.abedala@gmail.com>
2021-01-12 01:02:45 -0500

    Update files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./components/home";
 import Nav from "./components/Nav";
+import Home from "./components/home";
 import About from "./components/about";
 import Portfolio from "./components/portfolio";
 import Contact from "./components/contact";
